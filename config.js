aa4ee5398ff815281ba9c81c6f954497819ce271
Cristina Rodriguez <cristina@Cristinas-MacBook-Air.local>
2017-11-30 21:05:38 -0800

    security

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    FB_PAGE_TOKEN: 'EAAB11kiTKcgBAOMLZCNTn8dEVhxY2zXQy1UElyyZAWzyRfn2umZB9Glcf81YFiHW6mSq55FPPxKHIVyL8KdvGAZB10XBDfNTngMTKS3ZBgpw5Ev1njZBZCCHuEdokSQJa5cxrZAYb0z28ejjUELO9hfmBjCjvQQnKDwmJ9S1vCMiTgZDZD',
-    FB_VERIFY_TOKEN: 'djfdhsjfhuergdasjnasmbcdjshgfkajhgoaidjdjhflks',
-    API_AI_CLIENT_ACCESS_TOKEN: '9cabd28550f94cc89be5707bcb684833',
-    FB_APP_SECRET: '20a85bd644222cdf86d4c8e65d53e6b5',
-    SERVER_URL: "https://phillthechatbot.herokuapp.com/",
-};
